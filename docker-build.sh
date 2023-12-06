4c742095e2e1bc13e7fc93e0a602153af2f0a35a
don Rumata <v0541k@yandex.ru>
2023-12-07 00:07:57 +0300

    Add Dockerfile and staff. Update README.md.

---
+++ docker-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# `REPOSITORY` in `docker images`
+JC_DOCKER_LOCAL_REPO="jc"
+# Before `/`
+JC_DOCKER_IMAGE_NAME="$(grep name ./render.yaml | awk '{print $2}')"
+# After `:`
+. ./.env
+
+docker build \
+    --tag "$JC_DOCKER_LOCAL_REPO"/"$JC_DOCKER_IMAGE_NAME":"$JC_DOCKER_IMAGE_VERSION" \
+    --build-arg PYTHON_RUNTIME_VERSION="$(cut -d '-' -f 2 ./runtime.txt)" .
